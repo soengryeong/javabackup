0a6673e66b3d01de1c31b4c1f4387e979c77c9d9
HU-05 <HU-05@192.168.0.94>
2020-02-21 16:11:42 +0900

    200221backup1

---
+++ src/com/human/ex/JavaStart00.java
@@ -0,0 +1,11 @@
+package com.human.ex;
+
+public class JavaStart00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world\nKim Bo Reum\nHuman Education Center");
+		System.out.println("hello world");
+	}
+
+}
