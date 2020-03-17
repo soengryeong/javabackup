2abc82aa5758846c8615386be2190d7c0a39bbe9
HU-05 <HU-05@192.168.0.94>
2020-03-17 16:00:19 +0900

    0317

--- src/quize22.java
+++ src/quize22.java
@@ -2,7 +2,12 @@
 public class quize22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 다음과 같이 0이 아닌 정수를 반복적으로 입력받고
+		// 입력받은 수 중에서 가장 큰 수와 가장 작은 수를 출력하는
+		// 프로그램을 do=while문을 이용하여 만들어보자. 
+		// 입력이 0일 경우 종료하시오.
+		
+		
 
 	}
 
